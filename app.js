1749c4cb488c2e206205293ff0ebbc397b275041
raultilves <raulsantanatilves710@gmail.com>
2020-01-30 21:14:03 +0000

    Pruebas ENV 2

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  //res.send('Hello World!');
-  res.send(process.env.TIMES);
+  res.send('Hello World!');
+  console.log(process.env.TIMES);
 });
 
 let port = process.env.PORT;
